10cb3ed3e4fe99f089e02d589161cad52179370f
rifqiawali14 <rifqiawali14@gmail.com>
2025-08-20 13:22:25 +0700

    testing login dan penambahan layout center

---
+++ app/Views/layouts/center.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?? 'Halaman' ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ app/Views/login/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voting - Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title text-center mb-4">
+                </h2>
+                <form action="">
+                    <div class="form-floating mb-3">
+                        <input id="user" type="text" class="form-control" placeholder="username" required>
+                        <label for="user">Username</label>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input id="password" type="password" class="form-control" placeholder="password" required>
+                        <label for="password">Password</label>
+                    </div>
+
+                    <div class="d-grid">
+                        <button class="btn btn-primary btn-lg" type="submit">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
